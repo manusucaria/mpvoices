af9d71b4fecf95eea3ac6c3c03a13e27695c52bb
Manuel Sucaria <manusucaria@gmail.com>
2023-09-04 00:17:31 -0300

    4 del 9 1

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,21 @@ export default function Home() {
           setAlumnos(data)
       })
   }, []);
-  console.log(alumnos)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div>
         <p>Hola Voices</p>
+        {alumnos.length >= 1 ? 
+          <div>
+            {alumnos.map((alumno) => (
+              <div key={alumno.nombre}>
+                <p>{alumno.nombre}</p>
+                <p>{alumno.edad}</p>
+                <p>{alumno.instrumento}</p>
+              </div>
+            ))}
+          </div>
+        : ""}
       </div>
     </main>
   )
