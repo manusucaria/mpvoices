c5f995b686bf1bf93e4eae94a68ffe7073ca5df2
Aldair González <aldairgome97@gmail.com>
2023-09-26 11:28:48 -0600

    Limpiando mesa de trabajo

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,7 @@
-'use client'
-import { getArticle } from './api/api'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 export default function Home () {
-  const [alumnos, setAlumnos] = useState([])
-  useEffect(() => {
-    getArticle().then(data => {
-      setAlumnos(data)
-    })
-  }, [])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div>
-        <p>Hola Voices</p>
-        {alumnos.length >= 1
-          ? <div>
-            {alumnos.map((alumno) => (
-              <div key={alumno.nombre}>
-                <p>{alumno.nombre}</p>
-                <p>{alumno.edad}</p>
-                <p>{alumno.instrumento}</p>
-              </div>
-            ))}
-          </div>
-          : ''}
-      </div>
-    </main>
+    <main></main>
   )
 }
