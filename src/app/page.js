0e0852760668fbfd12268296d0d17a207354b45d
Aldair González <aldairgome97@gmail.com>
2023-09-27 08:02:16 -0600

    Se creó la sección de Nuevos Proyectos

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SectionClases from './sections/section-clases/SectionClases'
 import SectionTalleres from './sections/section-talleres/SectionTalleres'
+import SectionNuevosProyectos from './sections/section-nuevos-proyectos/SectionNuevosProyectos'
 
 export default async function Home () {
   return (
     <main>
       <SectionTalleres />
       <SectionClases />
+      <SectionNuevosProyectos />
     </main>
   )
 }
