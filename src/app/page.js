218b7fe7507eff7b08e768b9a607e9547b97b2fa
Aldair González <aldairgome97@gmail.com>
2023-09-28 12:29:29 -0600

    Se creó la sección hero bottom

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import SectionClases from './sections/section-clases/SectionClases'
 import SectionTalleres from './sections/section-talleres/SectionTalleres'
 import SectionNuevosProyectos from './sections/section-nuevos-proyectos/SectionNuevosProyectos'
 import SectionNovedades from './sections/section-novedades/SectionNovedades'
+import SectionHeroBottom from './sections/section-hero-bottom/SectionHeroBottom'
 
 export default async function Home () {
   return (
@@ -10,6 +11,7 @@ export default async function Home () {
       <SectionTalleres />
       <SectionClases />
       <SectionNuevosProyectos />
+      <SectionHeroBottom />
       <SectionNovedades />
     </main>
   )
