4de57887ec27a3c8cde6d64dfa22cf5b0c60a76d
Aldair González <aldairgome97@gmail.com>
2023-10-03 18:37:31 -0600

    Creando el slide bottom

--- src/app/sections/section-hero-bottom/SectionHeroBottom.jsx
+++ src/app/sections/section-hero-bottom/SectionHeroBottom.jsx
@@ -1,23 +1,70 @@
+'use client'
 import React from 'react'
 
+import { v4 as uuidv4 } from 'uuid'
 import Image from 'next/image'
-
 import './SectionHeroBottom.scss'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, EffectFade } from 'swiper/modules'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/effect-fade'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+const images = [
+  {
+    id: uuidv4(),
+    src: '/assets/static/slide/Imagen1Bottom-Desktop.jpg',
+    alt: 'Imagen 1',
+    isVisible: false
+  },
+  {
+    id: uuidv4(),
+    src: '/assets/static/slide/Imagen2Bottom-Desktop.jpg',
+    alt: 'Imagen 2',
+    isVisible: false
+  },
+  {
+    id: uuidv4(),
+    src: '/assets/static/slide/Imagen3Bottom-Desktop.jpg',
+    alt: 'Imagen 3',
+    isVisible: false
+  }
+]
 
 function SectionHeroBottom () {
   return (
     <div className='Hero'>
-      <Image
-        className='Hero-image'
-        src='/assets/static/hero-bottom.png'
-        alt='hero-bottom'
-        loading='lazy'
-        width={1000}
-        height={1000}
-      />
-      <div className="Hero-gradient">
-        <div className="Hero-gradient--content"></div>
-      </div>
+    <Swiper
+        spaceBetween={30}
+        effect="fade"
+        loop="true"
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false
+        }}
+        modules={[EffectFade, Autoplay]}
+        className="mySwiper"
+    >
+      {
+        images.map((image) => (
+          <SwiperSlide
+            key={image.id}
+          >
+            <Image
+              src={image.src}
+              alt={image.alt}
+              className='Hero-img'
+              loading='lazy'
+              width={1000}
+              height={1000}
+            />
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
     </div>
   )
 }
