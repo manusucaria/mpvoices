7396acca46cc153e79f00e74f83786095140bba9
Manuel Sucaria <manusucaria@gmail.com>
2023-10-18 12:26:12 -0300

    correcciones 18 del 10

--- src/app/sections/section-separador-alas/SeparadorAlas.js
+++ src/app/sections/section-separador-alas/SeparadorAlas.js
@@ -7,8 +7,8 @@ const SeparadorAlas = () => {
     <div className='grid grid-cols-1 grid-rows-1 w-auto h-auto'>
       <div className='bg-black col-start-1 col-end-1 row-start-1 row-end-1 z-40 border-2 lg:border-4 border-white opacity-30 h-[90%] w-[90%] m-auto'></div>
       <div className='col-start-1 col-end-1 row-start-1 row-end-1 m-auto z-40 flex flex-col'>
-        <h2 className='text-3xl font-bold z-40 m-auto mb-2 lg:mb-4'>Voices le pone</h2>
-        <h2 className='text-3xl font-bold z-40 m-auto'>alas a tu música</h2>
+        <h2 className='text-3xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-5xl font-bold z-40 m-auto mb-2 lg:mb-4'>Voices le pone</h2>
+        <h2 className='text-3xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-5xl font-bold z-40 m-auto'>alas a tu música</h2>
       </div>
       <Image
         width={500}
