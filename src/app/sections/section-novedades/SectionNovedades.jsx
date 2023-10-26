db4373c2206e9e04fc5819802067c00e2e5075e5
Aldair González <aldairgome97@gmail.com>
2023-10-26 12:21:04 -0600

    Ajustando el margen del boton de la sección Novedades

--- src/app/sections/section-novedades/SectionNovedades.jsx
+++ src/app/sections/section-novedades/SectionNovedades.jsx
@@ -11,20 +11,22 @@ function SectionNovedades () {
   return (
     <>
       <section className="Novedades">
-        <Wrapper className="flex flex-col justify-center items-center gap-5">
-          <h2 className={`Novedades-title ${playfair700.className}`}>Novedades</h2>
-          <div className="Novedades-ilustration">
-            <Image
-              className="Novedades-image"
-              src="/assets/static/Voices_World-c1.png"
-              loading='lazy'
-              alt='maríapeña voices world'
-              width={1000}
-              height={1000}
-            />
+        <Wrapper className="flex flex-col justify-center items-center gap-16">
+          <div className="flex flex-col justify-center items-center gap-5">
+            <h2 className={`Novedades-title ${playfair700.className}`}>Novedades</h2>
+            <div className="Novedades-ilustration">
+              <Image
+                className="Novedades-image"
+                src="/assets/static/Voices_World-c1.png"
+                loading='lazy'
+                alt='maríapeña voices world'
+                width={1000}
+                height={1000}
+              />
+            </div>
+            <p className={`Novedades-sub ${playfair400.className} text-2xl`}>Voices World</p>
+            <p className={`Novedades-p ${openSans600.className}`}>Te acompañamos con clases online donde estés, en cualquier parte del mundo</p>
           </div>
-          <p className={`Novedades-sub ${playfair400.className} text-2xl`}>Voices World</p>
-          <p className={`Novedades-p ${openSans600.className}`}>Te acompañamos con clases online donde estés, en cualquier parte del mundo</p>
 
           <Button text="Enterate en Instagram" path='https://www.instagram.com/voicesworld/' />
         </Wrapper>
