8cdf84c3d1d1be8837bc641673cdc0aaeb8aff7d
Aldair González <aldairgome97@gmail.com>
2023-10-26 12:18:18 -0600

    Ajustando la fuente de la sección Novedades

--- src/app/sections/section-novedades/SectionNovedades.jsx
+++ src/app/sections/section-novedades/SectionNovedades.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Image from 'next/image'
-import { playfair700, playfair500, openSans600 } from '@/utils/fonts/fonts'
+import { playfair700, openSans600, playfair400 } from '@/utils/fonts/fonts'
 import Button from '@/app/components/button/Button'
 import Wrapper from '@/app/components/wrapper/Wrapper'
 
@@ -23,7 +23,7 @@ function SectionNovedades () {
               height={1000}
             />
           </div>
-          <p className={`Novedades-sub ${playfair500.className}`}>Voices World</p>
+          <p className={`Novedades-sub ${playfair400.className} text-2xl`}>Voices World</p>
           <p className={`Novedades-p ${openSans600.className}`}>Te acompañamos con clases online donde estés, en cualquier parte del mundo</p>
 
           <Button text="Enterate en Instagram" path='https://www.instagram.com/voicesworld/' />
