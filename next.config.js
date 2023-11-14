afe43d5a0f7ec577274a0e1a2db74cf4d647caed
Aldair González <aldairgome97@gmail.com>
2023-11-14 17:01:34 -0600

    Añadiendo un proveedor de imágenes externo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'scontent.cdninstagram.com',
+        protocol: 'https'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
