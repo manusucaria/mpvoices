e963fa0b7ad698d6e0bd1a80c8fefebe2cccd3b4
Manuel Sucaria <manusucaria@gmail.com>
2023-11-18 12:17:58 -0300

    correccion undefined en insta

--- src/app/sections/section-insta/SectionInsta.jsx
+++ src/app/sections/section-insta/SectionInsta.jsx
@@ -10,8 +10,17 @@ const fetchData = async () => {
   const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.INSTAGRAM_KEY}`
   const data = await fetch(url)
   const res = await data.json()
-  const images = res.data.filter((item) => item.media_type === 'IMAGE')
-  return images.slice(0, 2)
+
+  // Verificar si res está definido y tiene la propiedad data
+  if (res && res.data) {
+    // Filtrar solo las imágenes
+    const images = res.data.filter((item) => item.media_type === 'IMAGE')
+    return images.slice(0, 2)
+  } else {
+    // Manejar el caso en que no se obtengan los datos correctamente
+    console.error('Error obteniendo datos de Instagram:', res)
+    return [] // o maneja de otra manera el caso de error
+  }
 }
 
 async function SectionInsta () {
