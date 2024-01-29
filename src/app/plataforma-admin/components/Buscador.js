5cd0bec0580289aff1e90a35a399063cf37c79d3
Manuel Sucaria <manusucaria@gmail.com>
2024-01-29 12:03:15 -0300

    plataforma admin buscador y agenda

---
+++ src/app/plataforma-admin/components/Buscador.js
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react'
+import { getAlumnos, getProfesores } from '../../api/api.js'
+import { useAuth } from '../../../lib/auth.js'
+
+const Buscador = () => {
+  const user = useAuth()
+  const [alumnos, setAlumnos] = useState([])
+  const [profesores, setProfesores] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [selectedAlumno, setSelectedAlumno] = useState(null)
+  const [selectedProfesor, setSelectedProfesor] = useState(null)
+
+  useEffect(() => {
+    getAlumnos().then(data => {
+      setAlumnos(data)
+    })
+    getProfesores().then(data => {
+      setProfesores(data)
+    })
+  }, [user])
+
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value)
+    clearDetails()
+  }
+
+  const handleAlumnoClick = (alumno) => {
+    setSelectedAlumno(alumno)
+    setSearchTerm('')
+  }
+
+  const handleProfesorClick = (profesor) => {
+    setSelectedProfesor(profesor)
+    setSearchTerm('')
+  }
+
+  const clearDetails = () => {
+    setSelectedAlumno(null)
+    setSelectedProfesor(null)
+  }
+
+  const buscarAlumnos = searchTerm.trim() !== ''
+    ? alumnos.filter(alumno => (
+      alumno.Nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      alumno.Apellido.toLowerCase().includes(searchTerm.toLowerCase())
+    ))
+    : []
+
+  const buscarProfesores = searchTerm.trim() !== ''
+    ? profesores.filter(profesor => (
+      profesor.Nombre.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      profesor.Apellido.toLowerCase().includes(searchTerm.toLowerCase())
+    ))
+    : []
+
+  return (
+    <div className='flex flex-col'>
+      <h2 className='text-center text-3xl sm:text-5xl mt-8 mb-4 text-white'>Buscar Perfiles</h2>
+      <input
+        type="text"
+        placeholder="Buscar alumnos o profesores"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
+        onClick={clearDetails}
+        className="text-black bg-white w-96 mx-auto text-center px-3 py-2 mb-4 rounded-3xl"
+      />
+      {searchTerm.trim() !== '' && (
+        <div className='flex flex-col mx-auto'>
+          {buscarAlumnos.map((alumno) => (
+            <div key={alumno.id} className="flex flex-col mx-auto my-8 w-100">
+              <button onClick={() => handleAlumnoClick(alumno)} className='bg-white text-black rounded-3xl h-8 w-96'>{alumno.Nombre} {alumno.Apellido}</button>
+            </div>
+          ))}
+        </div>
+      )}
+      {selectedAlumno && (
+        <div className="flex flex-col mx-auto px-3 py-2 items-center">
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Nombre: {selectedAlumno.Nombre}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Apellido: {selectedAlumno.Apellido}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Edad: {selectedAlumno.Edad}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Profesor: {selectedAlumno.Profesor}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Dia: {selectedAlumno.Dia}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Horario: {selectedAlumno.Horario}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Duración de Clase: {selectedAlumno.Duracion} minutos</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Instrumento: {selectedAlumno.Instrumento}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Deuda: {selectedAlumno.Deuda}</p>
+          <button onClick={clearDetails} className='bg-white text-black font-botones font-bold text-center text-sm rounded-3xl h-8 w-96'>Volver</button>
+        </div>
+      )}
+      {searchTerm.trim() !== '' && (
+        <div className='flex flex-col mx-auto'>
+          {buscarProfesores.map((profesor) => (
+            <div key={profesor.id} className="flex flex-col mx-auto my-8 w-100">
+              <button onClick={() => handleProfesorClick(profesor)} className='bg-white text-black rounded-3xl h-8 w-96'>{profesor.Nombre} {profesor.Apellido}</button>
+            </div>
+          ))}
+        </div>
+      )}
+      {selectedProfesor && (
+        <div className="flex flex-col mx-auto px-3 py-2 items-center">
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Nombre: {selectedProfesor.Nombre}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Apellido: {selectedProfesor.Apellido}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Edad: {selectedProfesor.Edad}</p>
+          <p className="text-white font-botones font-bold text-center text-sm mb-2">Instrumento: {selectedProfesor.Instrumento}</p>
+          <button onClick={clearDetails} className='bg-white text-black font-botones font-bold text-center text-sm rounded-3xl h-8 w-96'>Volver</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Buscador
--- src/app/plataforma-admin/page.js
+++ src/app/plataforma-admin/page.js
@@ -1,15 +1,14 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import { getAlumnos, getProfesores } from '../api/api.js'
 import { useRouter } from 'next/navigation'
 import { useAuth } from '../../lib/auth'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../lib/firebase.js'
+import Buscador from './components/buscador.js'
+import Agenda from './components/Agenda.js'
 
 const page = () => {
   const user = useAuth()
-  const [alumnos, setAlumnos] = useState([])
-  const [profesores, setProfesores] = useState([])
   const [usuario, setUsuario] = useState('')
   const [userLoaded, setUserLoaded] = useState(false)
   const router = useRouter()
@@ -18,12 +17,6 @@ const page = () => {
     const userType = localStorage.getItem('usuario')
     setUsuario(userType)
     if (user) {
-      getAlumnos().then(data => {
-        setAlumnos(data)
-      })
-      getProfesores().then(data => {
-        setProfesores(data)
-      })
       setUserLoaded(true)
       if (usuario !== 'admin') {
         setUserLoaded(false)
@@ -41,47 +34,19 @@ const page = () => {
       router.push('/')
     })
   }
+
   return (
     <div className='flex flex-col'>
-      <h1 className='text-center text-white text-3xl sm:text-5xl mt-6 mb-12'>Plataforma Administración</h1>
-      <h2 className='text-center text-3xl sm:text-5xl mb-4 text-white'>Alumnos</h2>
-      {alumnos.length >= 1
-        ? <div className='flex flex-col mx-auto'>
-            {alumnos.map((alumno) => (
-              <div key={alumno.id} className="flex flex-col mx-auto my-8">
-                <div className="flex flex-col mx-auto px-3 py-2 items-center">
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Nombre: {alumno.Nombre}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Apellido: {alumno.Apellido}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Edad: {alumno.Edad}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Profesor: {alumno.Profesor}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Dia: {alumno.Dia}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Horario: {alumno.Horario}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Duración de Clase: {alumno.Duracion} minutos</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Instrumento: {alumno.Instrumento}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Deuda: {alumno.Deuda}</p>
-                </div>
-              </div>
-            ))}
+      {user
+        ? <div>
+        <h1 className='text-center text-white text-3xl sm:text-5xl mt-6 mb-12'>¡Bienvenido Administrador!</h1>
+        <Agenda />
+        <Buscador />
+        <div className='mx-auto bg-white text-black p-2 my-6 w-96 h-auto text-center rounded-3xl hover:cursor-pointer' onClick={handleLogout}>
+          <p>Cerrar Sesión</p>
         </div>
-        : ''}
-      <h2 className='text-center text-3xl sm:text-5xl mt-8 mb-4 text-white'>Profesores</h2>
-      {profesores.length >= 1
-        ? <div className='flex flex-col mx-auto'>
-            {profesores.map((profe) => (
-              <div key={profe.id} className="flex flex-col mx-auto my-8">
-                <div className="flex flex-col mx-auto px-3 py-2 items-center">
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Nombre: {profe.Nombre}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Apellido: {profe.Apellido}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Edad: {profe.Edad}</p>
-                  <p className="text-white font-botones font-bold text-center text-sm mb-2">Instrumento: {profe.Instrumento}</p>
-                </div>
-              </div>
-            ))}
-        </div>
-        : ''}
-      <div className='mx-auto border p-2 my-6 border-white w-auto h-auto rounded-3xl' onClick={handleLogout}>
-        <p>Cerrar Sesión</p>
       </div>
+        : ''}
     </div>
   )
 }
