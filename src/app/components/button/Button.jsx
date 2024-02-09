1be8c2836ce92cec9c744e177da43bf46ff5e092
Aldair Gonzalez <aldairgome97@gmail.com>
2024-02-08 18:44:49 -0600

    Añadiendo props extras al componente button

--- src/app/components/button/Button.jsx
+++ src/app/components/button/Button.jsx
@@ -4,7 +4,33 @@ import './Button.scss'
 import Link from 'next/link'
 import { openSans700 } from '@/utils/fonts/fonts'
 
-function Button ({ text, mode, alterColor, path }) {
-  return <Link href={path} target='_blank' className={`Button ${mode ? `Button-${mode}` : ''} ${openSans700.className} ${alterColor ? 'Button-alter' : ''}`}>{ text }</Link>
+function Button ({ target, text, mode, alterColor, path, hasACallback, ...props }) {
+  return (
+    <>
+      {hasACallback
+        ? (
+        <button
+          className={`Button ${mode ? `Button-${mode}` : ''} ${
+            openSans700.className
+          } ${alterColor ? 'Button-alter' : ''}`}
+          {...props}
+        >
+          {text}
+        </button>
+          )
+        : (
+        <Link
+          href={path}
+          target={`${target ? '_blank' : ''}`}
+          className={`Button ${mode ? `Button-${mode}` : ''} ${
+            openSans700.className
+          } ${alterColor ? 'Button-alter' : ''}`}
+          {...props}
+        >
+          {text}
+        </Link>
+          )}
+    </>
+  )
 }
 export default Button
