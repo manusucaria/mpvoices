460e234e1b3135717fe0b28dcee6f0c18124c800
Manuel Sucaria <manusucaria@gmail.com>
2024-02-28 18:14:16 -0300

    agendaSmall

--- src/app/plataforma-admin/components/AgendaSmall.js
+++ src/app/plataforma-admin/components/AgendaSmall.js
@@ -178,20 +178,20 @@ const AgendaSmall = ({ cambios }) => {
             )}
           </div>
           <div className='grid grid-cols-[20%_80%] grid-rows-37 border-y-[0.5px] sm:border-y-1 border-y-[#0D0D0D]'>
-            <div className="bg-[#E9500E] h-12 text-center col-start-1 col-end-2 row-start-1 row-end-2 flex border-b-[0.5px] sm:border-b-1 border-b-[#0D0D0D]">
+            <div className="bg-[#E9500E] h-12 border-r-[0.5px] sm:border-r-1 border-r-[#0D0D0D] text-center col-start-1 col-end-2 row-start-1 row-end-2 flex border-b-[0.5px] sm:border-b-1 border-b-[#0D0D0D]">
               <p className='text-md sm:text-md md:text-base m-auto text-[#FFFFFF]'>Hora</p>
             </div>
             {timeSlots.map((time, index) => (
               <div
                 key={index}
-                className={`text-center col-start-1 col-end-2 bg-[#FFFFFF] flex h-12 ${index === timeSlots.length - 1 ? 'border-b-none rounded-bl-2xl' : 'border-b-[0.5px] sm:border-b-1 border-b-[#0D0D0D]'}`}
+                className={`text-center col-start-1 col-end-2 bg-[#FFFFFF] border-r-[0.5px] sm:border-r-1 border-r-[#0D0D0D] flex h-12 ${index === timeSlots.length - 1 ? 'border-b-none rounded-bl-2xl' : 'border-b-[0.5px] sm:border-b-1 border-b-[#0D0D0D]'}`}
               >
                 <p className='text-md sm:text-md md:text-base m-auto text-[#0D0D0D]'>{time}</p>
               </div>
             ))}
             <div className="col-start-2 col-end-3 row-start-1 row-end-38 grid grid-cols-1 bg-[#FFFFFF] rounded-br-2xl">
               {filteredProfesoresSorted.slice(startIndex, startIndex + 1).map((profesor) => (
-              <div key={profesor.id} className='grid grid-cols-1 grid-rows-37 border-l-[0.5px] sm:border-l-1 border-l-[#0D0D0D] text-center'>
+              <div key={profesor.id} className='grid grid-cols-1 grid-rows-37 text-center'>
                 <div style={{ backgroundColor: `rgba(102, 52, 129, ${backgroundColorAlpha})` }} className='flex row-start-1 row-end-2 h-12 text-sm border-b-[0.5px] sm:border-b-1 border-b-[#0D0D0D]'>
                   <p className='text-md sm:text-md md:text-base m-auto'>Profesor: {profesor.Nombre} / {profesor.Instrumento}</p>
                 </div>
