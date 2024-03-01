0e4be67cd55a7af3eb245632bcd67094aac81008
Manuel Sucaria <manusucaria@gmail.com>
2024-03-01 20:37:18 -0300

    css 2

--- src/app/plataforma-admin/components/AgendaFull.js
+++ src/app/plataforma-admin/components/AgendaFull.js
@@ -12,6 +12,8 @@ const AgendaFull = ({ cambios }) => {
   const [filteredAlumnos, setFilteredAlumnos] = useState([])
   const [filteredProfesores, setFilteredProfesores] = useState([])
   const [backgroundColorAlpha, setBackgroundColorAlpha] = useState(1)
+  const [showNotification, setShowNotification] = useState(false)
+  const [notification, setNotification] = useState([])
 
   useEffect(() => {
     getAlumnos().then((data) => {
@@ -101,6 +103,16 @@ const AgendaFull = ({ cambios }) => {
     const nuevaHora = Math.floor(nuevoHorarioMinutos / 60)
     const nuevoMinuto = nuevoHorarioMinutos % 60
     return `${nuevaHora}:${nuevoMinuto < 10 ? '0' : ''}${nuevoMinuto}`
+  }
+
+  const handleAlumnoClick = (alumno) => {
+    setNotification(alumno.Notificaciones)
+    setShowNotification(true)
+  }
+
+  const closeNotification = () => {
+    setNotification([])
+    setShowNotification(false)
   }
 
   return (
@@ -209,9 +221,9 @@ const AgendaFull = ({ cambios }) => {
                       >
                         <p className='text-sm sm:text-sm md:text-base mt-auto font-bold pt-2 text-[#0D0D0D]'>Alumno: {alumno.Nombre} {alumno.Apellido}</p>
                         <p className='text-sm sm:text-sm md:text-base mb-auto text-[#0D0D0D]'>{alumno.Instrumento} {alumno.Horario}-{calcularNuevoHorario(alumno.Horario, alumno.Duracion)}hs</p>
-                        <div className='ms-auto pb-1 pe-2 sm:pe-4'>
+                        <div className='ms-auto pb-2 pe-2 sm:pe-4'>
                           {alumno.Notificaciones
-                            ? <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            ? <svg className='cursor-pointer' onClick={() => handleAlumnoClick(alumno)} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.55 2.08L4.12 0.65C1.72 2.48 0.14 5.3 0 8.5H2C2.15 5.85 3.51 3.53 5.55 2.08ZM17.94 8.5H19.94C19.79 5.3 18.21 2.48 15.82 0.65L14.4 2.08C16.42 3.53 17.79 5.85 17.94 8.5ZM15.97 9C15.97 5.93 14.33 3.36 11.47 2.68V2C11.47 1.17 10.8 0.5 9.97 0.5C9.14 0.5 8.47 1.17 8.47 2V2.68C5.6 3.36 3.97 5.92 3.97 9V14L1.97 16V17H17.97V16L15.97 14V9ZM9.97 20C10.11 20 10.24 19.99 10.37 19.96C11.02 19.82 11.55 19.38 11.81 18.78C11.91 18.54 11.96 18.28 11.96 18H7.96C7.97 19.1 8.86 20 9.97 20Z" fill="#D0242A"/>
                               </svg>
                             : <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -224,6 +236,19 @@ const AgendaFull = ({ cambios }) => {
                   ))}
                 </div>
               ))}
+{showNotification && notification.length > 0 && (
+  <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-[100]'>
+    <div className='p-12 rounded-lg text-center flex flex-col gap-y-8 bg-[#E9500E] border-1 border-[#0D0D0D]'>
+      <p className='text-[#FFFFFF] font-bold text-xl mx-auto'>Notificaciones:</p>
+      <div className='flex flex-col mx-auto'>
+        {notification.map((item, index) => (
+          <div key={index} className='text-[#FFFFFF] mr-auto'>- {item}</div>
+        ))}
+      </div>
+      <button className='text-[#FFFFFF] font-bold md:hover:text-[#DB9B6D]' onClick={closeNotification}>Cerrar</button>
+    </div>
+  </div>
+)}
           </div>
         </div>
       </div>
