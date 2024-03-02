1d801a3c33760b5d52afc1335505e8cfa24952e6
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-02 16:18:50 -0600

    Usando configuración de firebase

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { firebaseConfig } from '@/config/config'
 
 const config = {
-  apiKey: firebaseConfig.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: firebaseConfig.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: firebaseConfig.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: firebaseConfig.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: firebaseConfig.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: firebaseConfig.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: firebaseConfig.API_KEY,
+  authDomain: firebaseConfig.AUTH_DOMAIN,
+  projectId: firebaseConfig.PROJECT_ID,
+  storageBucket: firebaseConfig.STORAGE_BUCKET,
+  messagingSenderId: firebaseConfig.MESSAGING_SENDER_ID,
+  appId: firebaseConfig.APP_ID
 }
 
 const app = getApps().length > 0 ? getApp() : initializeApp(config)
