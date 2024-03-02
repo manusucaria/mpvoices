608bafde4e51a0a5d7d108e310944a56a705c966
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-02 16:53:43 -0600

    Api para el manejo de las cookies de sesion

---
+++ src/app/api/auth/session/route.js
@@ -0,0 +1,78 @@
+import { auth } from 'firebase-admin'
+
+import { cookies, headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+
+import firebaseConfig from '@/config/firebase.config'
+import { runAdminApp } from '@/lib/firebase/firebase-admin'
+
+runAdminApp()
+
+// Obtener una sesión
+export const GET = async () => {
+  try {
+    const cookieSession = cookies().get(
+      firebaseConfig.COOKIE_SESSION_NAME
+    )?.value
+    const sessionCookie = await auth().verifySessionCookie(cookieSession)
+
+    return NextResponse.json({
+      isLogged: true,
+      rol: sessionCookie.rol,
+      email_verified: sessionCookie.email_verified
+    })
+  } catch (error) {
+    return NextResponse.json(
+      { isLogged: false, error: error.message },
+      { status: 500 }
+    )
+  }
+}
+
+// Crear una sesión
+export const POST = async () => {
+  try {
+    const idToken = headers().get('Authorization').split('Bearer ')[1]
+    const decodedIdToken = await auth().verifyIdToken(idToken)
+
+    if (!(new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60)) {
+      return NextResponse.json(
+        { isLogged: false, error: 'Token expirado' },
+        { status: 401 }
+      )
+    }
+
+    const expiresIn = 60 * 60 * 24 * 5 * 1000
+    const sessionCookie = await auth().createSessionCookie(idToken, {
+      expiresIn
+    })
+
+    cookies().set(firebaseConfig.COOKIE_SESSION_NAME, sessionCookie, {
+      httpOnly: true,
+      maxAge: expiresIn,
+      secure: true,
+      sameSite: 'strict'
+    })
+
+    return NextResponse.json({ isLogged: true })
+  } catch (error) {
+    return NextResponse.json(
+      { isLogged: false, error: error.message },
+      { status: 500 }
+    )
+  }
+}
+
+// Eliminar una sesión
+export const DELETE = async () => {
+  try {
+    cookies().delete(firebaseConfig.COOKIE_SESSION_NAME)
+
+    return NextResponse.json({ isLogged: false })
+  } catch (error) {
+    return NextResponse.json(
+      { isLogged: false, error: error.message },
+      { status: 500 }
+    )
+  }
+}
