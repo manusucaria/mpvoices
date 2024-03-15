7758f937353904cf3b2fe8d7c2c1c02267584dc8
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-15 15:49:33 -0600

    Añadiendo la propiedad birthdate al la creación de cuenta de usuario

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -62,6 +62,7 @@ export const signUp = async ({
       nombre: rest.nombre,
       apellido: rest.apellido,
       telefono: phoneNumber,
+      birthdate: rest.birthdate,
       rol: rolAsignado,
       email
     })
