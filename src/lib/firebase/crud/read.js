2ec2d9024b461faedc74b9decd916d2b04521394
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-15 15:50:13 -0600

    Creando un método para obtener un alumno por id

--- src/lib/firebase/crud/read.js
+++ src/lib/firebase/crud/read.js
@@ -111,3 +111,22 @@ export const getAllAlumnos = async ({ getUsuario, getRol } = {}) => {
     throw error
   }
 }
+
+export const getAlumnoById = async (uid, { getUsuario, getRol } = {}) => {
+  try {
+    const ref = doc(db, 'alumnos', uid)
+    const docSnap = await getDoc(ref)
+    if (docSnap.exists()) {
+      let usuario = docSnap.data().usuario
+      if (getUsuario && usuario.id) {
+        usuario = await getUsuarioById({ id: usuario.id })
+      }
+      if (getRol && getUsuario && usuario.rol && usuario.rol.id) {
+        usuario.rol = await getRolById({ id: usuario.rol.id })
+      }
+      return { ...docSnap.data(), id: docSnap.id, usuario }
+    }
+  } catch (error) {
+    throw error
+  }
+}
