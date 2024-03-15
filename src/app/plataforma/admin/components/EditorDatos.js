9a37f3d148f9bb0fda51a1d6141b93a5c1376278
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-15 15:53:13 -0600

    Prettier

--- src/app/plataforma/admin/components/EditorDatos.js
+++ src/app/plataforma/admin/components/EditorDatos.js
@@ -80,63 +80,69 @@ const EditorDatos = ({ alumno, setSelectedAlumno }) => {
   }
 
   return (
-    <div className='w-full'>
+    <div className="w-full">
       {editMode
         ? (
-        <div className='flex flex-col w-full bg-[#0D0D0D] px-4 sm:px-8 pb-8 pt-4'>
-          <form className='w-full mx-auto' onSubmit={handleSubmit}>
-            <div className='flex mb-6'>
-              <label className='font-bold mr-auto w-2/6'>Nombre:</label>
+        <div className="flex flex-col w-full bg-[#0D0D0D] px-4 sm:px-8 pb-8 pt-4">
+          <form className="w-full mx-auto" onSubmit={handleSubmit}>
+            <div className="flex mb-6">
+              <label className="font-bold mr-auto w-2/6">Nombre:</label>
               <input
-                className='text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto'
-                type='text'
-                name='nombre'
+                className="text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto"
+                type="text"
+                name="nombre"
                 value={nombre}
                 onChange={(e) => setNombre(e.target.value)}
               />
             </div>
-            <div className='flex mb-6'>
-              <label className='font-bold mr-auto w-2/6'>Apellido:</label>
+            <div className="flex mb-6">
+              <label className="font-bold mr-auto w-2/6">Apellido:</label>
               <input
-                className='text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto'
-                type='text'
-                name='apellido'
+                className="text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto"
+                type="text"
+                name="apellido"
                 value={apellido}
                 onChange={(e) => setApellido(e.target.value)}
               />
             </div>
-            <div className='flex mb-6'>
-              <label className='font-bold mr-auto w-2/6'>Fecha de nac.:</label>
+            <div className="flex mb-6">
+              <label className="font-bold mr-auto w-2/6">Fecha de nac.:</label>
               <input
-                className='text-[#0D0D0D] rounded-3xl h-8 px-2 w-4/6 ml-auto'
-                type='date'
-                name='fecha'
+                className="text-[#0D0D0D] rounded-3xl h-8 px-2 w-4/6 ml-auto"
+                type="date"
+                name="fecha"
                 value={birthdate}
                 onChange={(e) => setBirthdate(e.target.value)}
               />
             </div>
-            <div className='flex mb-6'>
-              <label className='font-bold mr-auto w-2/6'>E-Mail:</label>
+            <div className="flex mb-6">
+              <label className="font-bold mr-auto w-2/6">E-Mail:</label>
               <input
-                className='text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto'
-                type='text'
-                name='email'
+                className="text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto"
+                type="text"
+                name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <div className='flex mb-6'>
-              <label className='font-bold mr-auto w-2/6'>Teléfono:</label>
+            <div className="flex mb-6">
+              <label className="font-bold mr-auto w-2/6">Teléfono:</label>
               <input
-                className='text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto'
-                type='text'
-                name='tel'
+                className="text-[#0D0D0D] rounded-3xl h-8 pl-2 w-4/6 ml-auto"
+                type="text"
+                name="tel"
                 value={telefono}
                 onChange={(e) => setTelefono(e.target.value)}
               />
             </div>
-            <div className='flex w-full mx-auto mt-8 gap-x-4'>
-            <button className={`font-botones font-bold rounded-3xl w-3/6 bg-[#E9500E] text-[#FFFFFF] px-3 h-12 sm:h-10 md:hover:bg-[#DB9B6D] ${sending && 'opacity-50'}`} type='submit' disabled={sending}>
+            <div className="flex w-full mx-auto mt-8 gap-x-4">
+              <button
+                className={`font-botones font-bold rounded-3xl w-3/6 bg-[#E9500E] text-[#FFFFFF] px-3 h-12 sm:h-10 md:hover:bg-[#DB9B6D] ${
+                  sending && 'opacity-50'
+                }`}
+                type="submit"
+                disabled={sending}
+              >
                 {sending
                   ? (
                   <div
@@ -166,9 +172,9 @@ const EditorDatos = ({ alumno, setSelectedAlumno }) => {
                     )}
               </button>
               <button
-                className='font-botones font-bold rounded-3xl w-3/6 ml-auto bg-[#FFFFFF] text-[#0D0D0D] md:hover:text-[#E9500E] border-2 border-[#E9500E] px-3 h-12 sm:h-10'
+                className="font-botones font-bold rounded-3xl w-3/6 ml-auto bg-[#FFFFFF] text-[#0D0D0D] md:hover:text-[#E9500E] border-2 border-[#E9500E] px-3 h-12 sm:h-10"
                 onClick={cancelarClick}
-                type='button'
+                type="button"
               >
                 Cancelar
               </button>
@@ -177,49 +183,54 @@ const EditorDatos = ({ alumno, setSelectedAlumno }) => {
         </div>
           )
         : (
-        <div className='flex flex-col w-full mx-auto bg-[#0D0D0D]'>
-          <div className='px-4 sm:px-8 pb-8 w-full pt-4'>
-            <div className='mb-8 flex'>
-              <p className='mr-2 text-base font-bold'>Nombre:</p>
-              <p className='text-base'>{nombre}</p>
+        <div className="flex flex-col w-full mx-auto bg-[#0D0D0D]">
+          <div className="px-4 sm:px-8 pb-8 w-full pt-4">
+            <div className="mb-8 flex">
+              <p className="mr-2 text-base font-bold">Nombre:</p>
+              <p className="text-base">{nombre}</p>
             </div>
-            <div className='mb-8 flex'>
-              <p className='mr-2 text-base font-bold'>Apellido:</p>
-              <p className='text-base'>{apellido}</p>
+            <div className="mb-8 flex">
+              <p className="mr-2 text-base font-bold">Apellido:</p>
+              <p className="text-base">{apellido}</p>
             </div>
-            <div className='mb-8 flex'>
-              <p className='mr-2 text-base font-bold'>Fecha de nac.:</p>
-              <p className='text-base'>{formatDate(birthdate)}</p>
+            <div className="mb-8 flex">
+              <p className="mr-2 text-base font-bold">Fecha de nac.:</p>
+              <p className="text-base">{formatDate(birthdate)}</p>
             </div>
-            <div className='mb-8 flex'>
-              <p className='mr-2 text-base font-bold'>E-Mail:</p>
-              <p className='text-base'>{email}</p>
+            <div className="mb-8 flex">
+              <p className="mr-2 text-base font-bold">E-Mail:</p>
+              <p className="text-base">{email}</p>
             </div>
-            <div className='flex'>
-              <p className='mr-2 text-base font-bold'>Teléfono:</p>
-              <p className='text-base'>{telefono}</p>
+            <div className="flex">
+              <p className="mr-2 text-base font-bold">Teléfono:</p>
+              <p className="text-base">{telefono}</p>
             </div>
           </div>
-          <div className='bg-[#0D0D0D] flex flex-col mx-auto w-full'>
-            <button className='font-botones font-bold rounded-3xl w-4/6 sm:w-3/6 mx-auto h-12 sm:h-10 mb-8 bg-[#E9500E] md:hover:bg-[#DB9B6D]' onClick={handleEditClick}>
-                Editar datos
+          <div className="bg-[#0D0D0D] flex flex-col mx-auto w-full">
+            <button
+              className="font-botones font-bold rounded-3xl w-4/6 sm:w-3/6 mx-auto h-12 sm:h-10 mb-8 bg-[#E9500E] md:hover:bg-[#DB9B6D]"
+              onClick={handleEditClick}
+            >
+              Editar datos
             </button>
           </div>
         </div>
           )}
-        {showConfirmation && (
-          <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
-            <div className="bg-[#FFFFFF] p-12 rounded-lg text-center flex flex-col">
-              <p className="text-[#0D0D0D] text-xl mb-4 font-bold">Los cambios se guardaron correctamente.</p>
-              <button
-                className="text-[#E9500E] md:hover:text-[#DB9B6D] ml-auto font-bold"
-                onClick={handleCloseConfirmation}
-              >
-                Entendido
-              </button>
-            </div>
+      {showConfirmation && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
+          <div className="bg-[#FFFFFF] p-12 rounded-lg text-center flex flex-col">
+            <p className="text-[#0D0D0D] text-xl mb-4 font-bold">
+              Los cambios se guardaron correctamente.
+            </p>
+            <button
+              className="text-[#E9500E] md:hover:text-[#DB9B6D] ml-auto font-bold"
+              onClick={handleCloseConfirmation}
+            >
+              Entendido
+            </button>
           </div>
-        )}
+        </div>
+      )}
     </div>
   )
 }
