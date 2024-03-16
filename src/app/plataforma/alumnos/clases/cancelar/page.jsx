54265a72d7ed4ee1ce9f22584f755ae8ecf5dc52
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-16 15:55:31 -0600

    Pintando los días correspondientes al calendario

--- src/app/plataforma/alumnos/clases/cancelar/page.jsx
+++ src/app/plataforma/alumnos/clases/cancelar/page.jsx
@@ -1,16 +1,84 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { playfair600 } from '@/utils/fonts/fonts'
+
+import { getAlumnoById } from '@/lib/firebase/crud/read'
+import { useAuth } from '@/lib/firebase/useAuth'
+import { signOut } from '@/lib/firebase/auth'
+
+import Button from '@/app/components/button/Button'
+import Loader from '@/app/components/loader/Loader'
+
 import CardContainer from '../../components/CardContainer'
 import ButtonReturn from '../../components/ButtonReturn'
-import Button from '@/app/components/button/Button'
+import Calendario from '../../components/Calendario'
 
 const page = () => {
+  const user = useAuth()
+
+  const [alumno, setAlumno] = useState(null)
+  const [highlightedDays, setHighlightedDays] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const [selectedDate, setSelectedDate] = useState(null) // Estado para almacenar la fecha seleccionada
+
+  const getDaysInMonth = ({ month, year, dayOfWeek }) => {
+    const days = []
+    const daysOfWeek = [
+      'domingo',
+      'lunes',
+      'martes',
+      'miércoles',
+      'jueves',
+      'viernes',
+      'sábado'
+    ]
+    const dayOfWeekNumber = daysOfWeek.indexOf(dayOfWeek.toLowerCase())
+
+    const date = new Date(year, month, 1)
+    while (date.getMonth() === month) {
+      if (date.getDay() === dayOfWeekNumber) {
+        days.push(date.getDate())
+      }
+      date.setDate(date.getDate() + 1)
+    }
+    return days
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (user) {
+          const dataAlumno =
+            user?.uid && (await getAlumnoById(user.uid, { getUsuario: true }))
+          if (!dataAlumno) {
+            await signOut()
+            window.location.reload()
+          }
+          const days = getDaysInMonth({
+            month: new Date().getMonth(),
+            year: new Date().getFullYear(),
+            dayOfWeek: dataAlumno.clases.dia
+          })
+          setHighlightedDays(days)
+          setAlumno(dataAlumno)
+          setLoading(false)
+        }
+      } catch (error) {
+        await signOut()
+        window.location.reload()
+      }
+    })()
+  }, [user])
+
   const handleCancelarClase = () => {
     console.log('cancelar clase')
+    console.log(selectedDate)
   }
+
+  if (loading) return <Loader />
 
   return (
     <div className="w-full h-full flex flex-col items-center">
@@ -26,12 +94,21 @@ const page = () => {
         top
         button={
           <Button
-            text="Cancelar clase"
-            hasACallback
+            text={selectedDate ? 'Cancelar clase' : 'Seleccionar fecha'}
+            mode={!selectedDate ? 'disabled' : ''}
             onClick={handleCancelarClase}
+            disabled={!selectedDate}
+            hasACallback
           />
         }
-      ></CardContainer>
+      >
+        <Calendario
+          highlightedDays={highlightedDays}
+          clases={alumno.clases}
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+        />
+      </CardContainer>
     </div>
   )
 }
