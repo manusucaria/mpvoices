d0a9e700a822499e5485664c699caaf53e1af54e
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-16 11:56:52 -0600

    Validando si la card tiene un mensaje de advertencia

--- src/app/plataforma/alumnos/components/CardContainer.jsx
+++ src/app/plataforma/alumnos/components/CardContainer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const CardContainer = ({ children, title }) => {
+const CardContainer = ({ children, button, title, warning, top, bottom }) => {
   return (
     <div className="bg-black w-1/2 p-10 text-base flex flex-col items-center gap-10">
-      <h3 className="w-full text-start border-b-1 pb-1 text-xl">{title}</h3>
+      <div className="w-full">
+        <h3 className="w-full text-start border-b-1 pb-1 text-xl">{title}</h3>
+        {warning && top && (
+          <p className="w-full border-b-1 py-1">
+            Importante:{' '}
+            <span className="text-orange-300">
+              recordá que las clases sólo se pueden cancelar con más de 24 hs de
+              antelación.
+            </span>
+          </p>
+        )}
+      </div>
       {children}
+      {warning && bottom && (
+        <p className="w-full border-y-1 py-1">
+          Importante:{' '}
+          <span className="text-orange-300">
+            recordá que las clases sólo se pueden cancelar con más de 24 hs de
+            antelación.
+          </span>
+        </p>
+      )}
+      {button}
     </div>
   )
 }
