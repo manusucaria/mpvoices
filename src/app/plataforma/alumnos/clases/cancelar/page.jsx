25cb8b9a5649ae2a07c51c0059aa227282d071bd
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-16 11:57:07 -0600

    Pasando un boton por parámetro

--- src/app/plataforma/alumnos/clases/cancelar/page.jsx
+++ src/app/plataforma/alumnos/clases/cancelar/page.jsx
@@ -1,10 +1,17 @@
+'use client'
+
 import React from 'react'
 
 import { playfair600 } from '@/utils/fonts/fonts'
 import CardContainer from '../../components/CardContainer'
 import ButtonReturn from '../../components/ButtonReturn'
+import Button from '@/app/components/button/Button'
 
 const page = () => {
+  const handleCancelarClase = () => {
+    console.log('cancelar clase')
+  }
+
   return (
     <div className="w-full h-full flex flex-col items-center">
       <div className="w-full py-10 text-center flex items-center justify-center relative">
@@ -13,7 +20,18 @@ const page = () => {
           Cancelar clase
         </h2>
       </div>
-      <CardContainer title="Seleccionar clase a cancelar"></CardContainer>
+      <CardContainer
+        title="Seleccionar clase a cancelar"
+        warning
+        top
+        button={
+          <Button
+            text="Cancelar clase"
+            hasACallback
+            onClick={handleCancelarClase}
+          />
+        }
+      ></CardContainer>
     </div>
   )
 }
