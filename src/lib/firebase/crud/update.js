44d03f5bd68333806079efcf6d4ddee315752b6b
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-19 22:05:34 -0600

    Si el usuario cancela una clase que no corresponde con su horario, esta no se guardará dentro del array de clases canceladas

--- src/lib/firebase/crud/update.js
+++ src/lib/firebase/crud/update.js
@@ -1,16 +1,34 @@
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import { arrayRemove, arrayUnion, doc, getDoc, updateDoc } from 'firebase/firestore'
+import { format } from 'date-fns'
+import { es } from 'date-fns/locale'
 
 import { db } from '../firebase'
 import { getAlumnoById } from './read'
 
-export const updateAlumnoCancelarClase = async (uid, { fecha }) => {
+export const updateAlumnoCancelarClase = async (uid, { fecha, hora_inicio, duracion }) => {
   try {
     const alumnoRef = doc(db, 'alumnos', uid)
-
-    await updateDoc(alumnoRef, {
-      'clases.canceladas': arrayUnion({ fecha }),
-      'clases.notificaciones': arrayUnion({ fecha, tipo: 'cancelar' })
-    })
+    const alumnoSnap = await getDoc(alumnoRef)
+    const alumno = alumnoSnap.data()
+    const { clases } = alumno
+    const dayUpdated = format(fecha, 'eeee', { locale: es })
+    const day = clases.dia
+    if (
+      clases.hora_inicio === hora_inicio &&
+      clases.duracion === duracion &&
+      dayUpdated === day
+    ) {
+      await updateDoc(alumnoRef, {
+        'clases.agendadas': arrayRemove({ fecha, hora_inicio, duracion }),
+        'clases.canceladas': arrayUnion({ fecha, hora_inicio, duracion }),
+        'clases.notificaciones': arrayUnion({ fecha, tipo: 'cancelar' })
+      })
+    } else {
+      await updateDoc(alumnoRef, {
+        'clases.agendadas': arrayRemove({ fecha, hora_inicio, duracion }),
+        'clases.notificaciones': arrayUnion({ fecha, tipo: 'cancelar' })
+      })
+    }
 
     const alumnoUpdated = await getAlumnoById(uid, {
       getUsuario: true,
@@ -18,6 +36,7 @@ export const updateAlumnoCancelarClase = async (uid, { fecha }) => {
     })
     return alumnoUpdated
   } catch (error) {
+    console.log(error)
     throw error
   }
 }
