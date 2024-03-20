735cd1b7905f4e1309fbedeffaf8dcd711072f98
Aldair Gonzalez <aldairgome97@gmail.com>
2024-03-19 20:48:15 -0600

    Se creó un método que obtiene todos los alumnos de un profesor excluyendo a un alumno en específico

--- src/lib/firebase/crud/read.js
+++ src/lib/firebase/crud/read.js
@@ -116,6 +116,34 @@ export const getAllAlumnos = async ({ getUsuario, getRol, getProfesor } = {}) =>
   }
 }
 
+export const getAllAlumnosFromAProfesorExcludingCurrentAlumno = async (profesorUid, alumnoUid, { getUsuario, getRol, getProfesor } = {}) => {
+  try {
+    const profesorRef = doc(db, 'profesores', profesorUid)
+    const ref = collection(db, 'alumnos')
+    const q = query(ref, where('profesor', '==', profesorRef))
+    const querySnapshot = await getDocs(q)
+    const alumnos = []
+    for (const doc of querySnapshot.docs) {
+      let profesor = doc.data().profesor
+      let usuario = doc.data().usuario
+      if (doc.id === alumnoUid) continue
+      if (getUsuario && usuario.id) {
+        usuario = await getUsuarioById({ id: usuario.id })
+      }
+      if (getRol && getUsuario && usuario.rol && usuario.rol.id) {
+        usuario.rol = await getRolById({ id: usuario.rol.id })
+      }
+      if (getProfesor && profesor && profesor.id) {
+        profesor = await getProfesorById(profesor.id, { getUsuario: true })
+      }
+      alumnos.push({ ...doc.data(), id: doc.id, usuario, profesor })
+    }
+    return alumnos
+  } catch (error) {
+    throw error
+  }
+}
+
 export const getAlumnoById = async (uid, { getUsuario, getRol, getProfesor } = {}) => {
   try {
     const ref = doc(db, 'alumnos', uid)
