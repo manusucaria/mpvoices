6f7f5a7011adc6a9a90423c04bff692987870ffd
Manuel Sucaria <manusucaria@gmail.com>
2024-03-31 00:15:27 -0300

    fix nombre y apellido notificaciones

--- src/app/plataforma/admin/components/NotificacionAdmin.js
+++ src/app/plataforma/admin/components/NotificacionAdmin.js
@@ -80,7 +80,7 @@ const NotificacionAdmin = ({
                   {sortedNotificaciones.map((item, index) => (
                     <div key={index} className='text-[#0D0D0D] font-bold mx-auto'>
                       <p>
-                        - {item?.usuario?.usuario?.full_name?.nombre} {item?.usuario?.full_name?.apellido} {item.tipo === 'cancelada' ? 'no asistirá' : 'asistirá'} a la clase del {format(item.fecha.toDate(), 'dd/MM/yyyy')}
+                        - {item?.usuario?.nombre} {item?.usuario?.apellido} {item.tipo === 'cancelada' ? 'no asistirá' : 'asistirá'} a la clase del {format(item.fecha.toDate(), 'dd/MM/yyyy')}
                       </p>
                     </div>
                   ))}
