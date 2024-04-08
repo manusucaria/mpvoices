c0579ff358168ec0fca24649164561544a3dcea6
Manuel Sucaria <manusucaria@gmail.com>
2024-04-08 11:55:38 -0300

    fix tipografia gome

--- src/app/sections/section-talleres/SectionTalleres.jsx
+++ src/app/sections/section-talleres/SectionTalleres.jsx
@@ -9,19 +9,19 @@ function SectionTalleres () {
   return (
     <section className="Talleres">
       <Wrapper className={`flex flex-col items-center gap-6 sm:gap-12 ${playfair600.className}`}>
-        <h3 className="Talleres-h3 flex flex-col">
-          <span>Talleres de ensamble </span>
-          <span>para tocar con otros alumnos</span>
-        </h3>
+        <div className="Talleres-h3 flex flex-col">
+          <h2>Talleres de ensamble </h2>
+          <h2>para tocar con otros alumnos</h2>
+        </div>
         <div className='bg-white w-20 sm:w-36 h-0.5'></div>
-        <h3 className="Talleres-h3 flex flex-col">
-          Presentaciones en vivo
-        </h3>
+          <h2 className="Talleres-h3 flex flex-col">
+            Presentaciones en vivo
+          </h2>
         <div className='bg-white w-20 sm:w-36 h-0.5'></div>
-        <h3 className="Talleres-h3 flex flex-col">
-        <span>Grabaciones en estudio </span>
-        <span>profesional</span>
-        </h3>
+        <div className="Talleres-h3 flex flex-col">
+          <h2>Grabaciones en estudio </h2>
+          <h2>profesional</h2>
+        </div>
       </Wrapper>
     </section>
   )
